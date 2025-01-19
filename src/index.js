24d04119cc382dafdeb5dca6ab64cad0c450601d
Mateo <carciu98@gmail.com>
2025-01-19 18:38:57 +0100

    fix(deploy): fix deploy for ci cd

--- src/index.js
+++ src/index.js
@@ -42,6 +42,10 @@ const server = http.createServer((req, res) => {
 	routeHandler(req, res);
 });
 
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
-});
+if (require.main === module) {
+	server.listen(port, hostname, () => {
+		console.log(`Server running at http://${hostname}:${port}/`);
+	});
+}
+
+module.exports = server;
